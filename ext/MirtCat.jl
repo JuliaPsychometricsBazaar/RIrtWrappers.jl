0059e0cd73b195ed8fee4e2d13ebd5976d6e595e
Frankie Robertson <frankie@robertson.name>
2023-10-26 15:36:21 +0300

    Add MirtCat wrapper

---
+++ ext/MirtCat.jl
@@ -0,0 +1,88 @@
+module MirtCat
+
+using Random: Xoshiro
+using RCall
+using FittedItemBanks
+import ComputerAdaptiveTesting.Sim
+
+
+struct MirtCatDesign{T}
+    inner::T
+end
+
+function params_to_mirt(params)
+    params[:, 1] = -params[:, 1]
+    params[:, 4] .= 1.0 .- params[:, 4]
+    rcopy(R"""
+        library(mirtCAT)
+        mat <- $params
+        colnames(mat) <- c("d", "a1", "g", "u")
+        generate.mirt_object(mat, "4PL")
+    """)
+end
+
+function make_mirtcat(params, criteria, next_item)
+    mirt_params = params_to_mirt(params)
+    mirt_design = rcopy(R"""
+        mirtCAT(df=NULL, mo=$mirt_params, design_elements=TRUE, criteria=$criteria, next_item=$next_item)
+    """)
+    (MirtCatDesign(mirt_design), mirt_params)
+end
+
+function next_item(mirt_design::MirtCatDesign)
+    design = mirt_design.inner
+    return R"""
+        options(show.error.locations = TRUE)
+        findNextItem($design)
+    """
+end
+
+function next_with_rollback(mirt_design, new_item, new_response)
+    r_mirt_design = mirt_design.inner
+    new_r_mirt_design = R"""
+        old_r_mirt_design <- c(
+            person = mirt_design$person$copy(),
+            test = mirt_design$test,
+            design = mirt_design$design
+        )
+        updateDesign($r_mirt_design , new_item = $new_item, new_response = $new_response)
+    """
+    return (MirtCatDesign(old_r_mirt_design), MirtCatDesign(new_r_mirt_design))
+end
+
+function next(mirt_design, new_item, new_response)
+    r_mirt_design = mirt_design.inner
+    r_mirt_design = R"""
+        updateDesign($r_mirt_design, new_item = $new_item, new_response = $new_response)
+    """
+    return MirtCatDesign(r_mirt_design)
+end
+
+"""
+Run a given CatLoopConfig with a MaterializedDecisionTree
+"""
+function Sim.run_cat(cat_config::Sim.CatLoopConfig{RulesT}, ib_labels=nothing) where {RulesT <: MirtCatDesign}
+    (; rules, get_response, new_response_callback) = cat_config
+
+    first = true
+    while true
+        next_index = next_item(rules)
+        terminating = next_index === nothing
+        if !first && new_response_callback !== nothing
+            new_response_callback(responses, terminating)
+        end
+        if terminating
+            @debug "Met termination condition"
+            break
+        end
+        next_label = Sim.item_label(ib_labels, next_index)
+        @debug "Querying" next_label
+        response = get_response(next_index, next_label)
+        @debug "Got response" response
+        add_response!(responses, Response(ResponseType(item_bank), next_index, response))
+        first = false
+    end
+    (responses, ability_estimate(rules, responses))
+end
+
+end
