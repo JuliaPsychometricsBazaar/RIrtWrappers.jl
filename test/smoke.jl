f09b8f707bca96562845d383768a04f3ca1c23bf
Frankie Robertson <frankie@robertson.name>
2024-08-04 19:08:59 +0300

    Break smoke tests into testsets

--- test/smoke.jl
+++ test/smoke.jl
@@ -5,37 +5,45 @@ using RIrtWrappers: KernSmoothIRT
 using RIrtWrappers: Mirt
 using Optim
 
+
 rng = Xoshiro(42)
 
-const dich_fits = [
-    KernSmoothIRT.fit_ks_dichotomous,
+const dich_df = DataFrame(
+    Dict("Q$qidx" => rand(rng, 0:1, 10) for qidx in 1:10); copycols = false)
+
+const rand_ord_df = DataFrame(
+    Dict("Q$qidx" => rand(rng, 0:2, 10) for qidx in 1:10); copycols = false)
+
+const mirt_dich_fits = [
     Mirt.fit_2pl,
     Mirt.fit_3pl,
     Mirt.fit_4pl
 ]
 
-const dich_df = DataFrame(
-    Dict("Q$qidx" => rand(rng, 0:1, 10) for qidx in 1:10); copycols = false)
-
-for fitter in dich_fits
-    @test fitter(dich_df)[1] isa AbstractItemBank
+@testset "mirt dichotomous fits" begin
+    for fitter in mirt_dich_fits
+        @test fitter(dich_df)[1] isa AbstractItemBank
+    end
 end
 
-const rand_ord_df = DataFrame(
-    Dict("Q$qidx" => rand(rng, 0:2, 10) for qidx in 1:10); copycols = false)
+@testset "mirt ordinal models" begin
+    uniq_ord_df = copy(rand_ord_df)
+    for col in propertynames(uniq_ord_df)
+        uniq_ord_df[1:3, col] = [0, 1, 2]
+    end
 
-uniq_ord_df = copy(rand_ord_df)
-for col in propertynames(uniq_ord_df)
-    uniq_ord_df[1:3, col] = [0, 1, 2]
+    @test Mirt.fit_gpcm(uniq_ord_df)[1] isa AbstractItemBank
+
+    hetro_ord_df = copy(rand_ord_df)
+    hetro_ord_df[!, :Q4] = [0 for _ in 1:10]
+
+    @test Mirt.fit_gpcm(hetro_ord_df)[1] isa AbstractItemBank skip=true
+
+    hetro_ord_df[1, :Q4] = 2
+
+    @test Mirt.fit_gpcm(hetro_ord_df)[1] isa AbstractItemBank skip=true
 end
 
-@test Mirt.fit_gpcm(uniq_ord_df)[1] isa AbstractItemBank
-
-hetro_ord_df = copy(rand_ord_df)
-hetro_ord_df[!, :Q4] = [0 for _ in 1:10]
-
-@test Mirt.fit_gpcm(hetro_ord_df)[1] isa AbstractItemBank skip=true
-
-hetro_ord_df[1, :Q4] = 2
-
-@test Mirt.fit_gpcm(hetro_ord_df)[1] isa AbstractItemBank skip=true
+@testset "ksirt dichotomous fit" begin
+    @test KernSmoothIRT.fit_ks_dichotomous(dich_df)[1] isa AbstractItemBank
+end
