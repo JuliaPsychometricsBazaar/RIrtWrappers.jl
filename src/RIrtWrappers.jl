ddc999ad0cb6d70005989f03d55066d8789cad4c
Frankie Robertson <frankie@robertson.name>
2025-03-13 10:45:40 +0200

    Add require_mirtcat helper for getting extension module

--- src/RIrtWrappers.jl
+++ src/RIrtWrappers.jl
@@ -1,6 +1,19 @@
 module RIrtWrappers
 
+export require_mirtcat
+
 include("./Mirt.jl")
 include("./KernSmoothIRT.jl")
+
+function require_mirtcat()
+    MirtCat = Base.get_extension(@__MODULE__, :MirtCat)
+    if MirtCat === nothing
+        error(
+            "Failed to load extension module MirtCat. " *
+            "(Do you have ComputerAdaptiveTesting.jl in your environment?)"
+        )
+    end
+    return MirtCat
+end
 
 end
