6f169778cd436ef8d022dd266e90cd6ffa763ce1
Frankie Robertson <frankie@robertson.name>
2025-03-19 11:39:51 +0200

    Formatting

--- src/FixRCall.jl
+++ src/FixRCall.jl
@@ -10,7 +10,8 @@ const RCALL_UUID = UUID("6f49c342-dc21-5d91-9882-a32aef131414")
 CondaPkg.add("r")
 target_rhome = joinpath(CondaPkg.envdir(), "lib", "R")
 if Sys.iswindows()
-    target_libr = joinpath(target_rhome, "bin", Sys.WORD_SIZE==64 ? "x64" : "i386", "R.dll")
+    target_libr = joinpath(
+        target_rhome, "bin", Sys.WORD_SIZE == 64 ? "x64" : "i386", "R.dll")
 else
     target_libr = joinpath(target_rhome, "lib", "libR.$(Libdl.dlext)")
 end
